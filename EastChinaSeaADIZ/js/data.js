e81a18321b08d9bbecfe6fc29d8b34a7b33fb6ef
Zhao <32775389+linfengzhao@users.noreply.github.com>
2018-09-06 20:15:25 +0800

    East China Sea

---
+++ EastChinaSeaADIZ/js/data.js
@@ -0,0 +1,86 @@
+var AreaData = [{
+    "latitude": 27.465,
+    "longitude": 121.13,
+    "type": "border"
+}, {
+    "latitude": 28.3916666666667,
+    "longitude": 121.911666666667,
+    "type": "border"
+}, {
+    "latitude": 28.3983333333333,
+    "longitude": 121.916666666667,
+    "type": "border"
+}, {
+    "latitude": 28.8883333333333,
+    "longitude": 122.275,
+    "type": "border"
+}, {
+    "latitude": 30.1683333333333,
+    "longitude": 122.945,
+    "type": "border"
+}, {
+    "latitude": 30.725,
+    "longitude": 123.161666666667,
+    "type": "border"
+}, {
+    "latitude": 30.735,
+    "longitude": 123.156666666667,
+    "type": "border"
+}, {
+    "latitude": 31.4216666666667,
+    "longitude": 122.243333333333,
+    "type": "border"
+}, {
+    "latitude": 33.015,
+    "longitude": 121.64,
+    "type": "border"
+}, {
+    "latitude": 33.18333333333333,
+    "longitude": 121.78333333333333,
+    "type": "boundary"
+}, {
+    "latitude": 33.18333333333333,
+    "longitude": 125,
+    "type": "boundary"
+}, {
+    "latitude": 31,
+    "longitude": 128.33333333333334,
+    "type": "boundary"
+}, {
+    "latitude": 25.633333333333333,
+    "longitude": 125,
+    "type": "boundary"
+}, {
+    "latitude": 24.75,
+    "longitude": 123,
+    "type": "boundary"
+}, {
+    "latitude": 26.733333333333334,
+    "longitude": 120.96666666666667,
+    "type": "boundary"
+}];
+var FlightData = [{
+    "id": "flight1",
+    "path": [
+        [114.3000994801, 30.5952549331],
+        [139.6917064000, 35.6894875000]
+    ],
+    "type": "normal"
+}, {
+    "id": "flight2",
+    "path": [
+        [113.2590231400, 23.1317504000],
+        [121.5200760000, 25.0307240000],
+        [126.9779692000, 37.5665350000]
+    ],
+    "type": "normal"
+}, {
+    "id": "flight3",
+    "path": [
+        [127.7675010000, 26.3555561000],
+        [122.4976480000, 27.5836020000],
+        [124.6501320000, 31.1127810000],
+        [129.7168630000, 33.1971510000]
+    ],
+    "type": "abnormal"
+}];
